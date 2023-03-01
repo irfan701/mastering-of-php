e36cb2c2b2eca5a611bf6328b10b4b9f13f8c0be
irfan701 <irfanhossain701@gmail.com>
2023-03-01 21:36:13 +0600

    even or odd trace

--- Array/15.array_reduce.php
+++ Array/15.array_reduce.php
@@ -1,13 +1,17 @@
 <?php
 /*
  *  - অ্যারের ইউটিলিটি ফাংশনস - খুবই জরুরী - অ্যারে রিডিউস
- * 
+ *
  * array_reduce
  */
 
-$numbers=[1,2,3,4];
-function sum($oldValue,$currentValue){
-    return $oldValue+$currentValue;
+$numbers=[1,2,3,4,5,6,7,8,9,10];
+function sum($oldValue,$newValue){
+    if($newValue%2==0){
+        return $oldValue+$newValue;
+    }
+    return $oldValue;
+
 }
 
 $sum=array_reduce($numbers,'sum');
