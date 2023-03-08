c333e5dbd2438211542e451253ee727011dbd054
irfan701 <irfanhossain701@gmail.com>
2023-03-09 03:10:15 +0600

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,6 @@
         <div class="column column-60 column-offset-20">
             <h1>Welcome</h1>
             <code>php -S localhost:8000 -t C:\Users\dxbor\Documents\GitHub\mastering-of-php</code>
-
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae debitis, deleniti dignissimos
                 doloribus dolorum eos harum in ipsa ipsam laboriosam perspiciatis provident quam quas qui quisquam quo
                 quos ratione rem rerum saepe sint tempora unde voluptates voluptatibus? Id maxime molestias sapiente
@@ -29,10 +28,7 @@
                 omnis possimus tempora ullam voluptates. Accusamus aliquid, assumenda beatae consequatur consequuntur
                 cupiditate dolore dolorum fugit numquam odio officia quo, totam vero.</p>
         </div>
-
     </div>
-
-
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
