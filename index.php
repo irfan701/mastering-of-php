9874b68aa3f61cb3fe32594acf02c2a3109fc2d6
irfan701 <irfanhossain701@gmail.com>
2023-03-09 00:02:24 +0600

    Builtin web server run

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        margin-top: 30px;
+    }
+</style>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="column column-60 column-offset-20">
+            <h1>Welcome</h1>
+            <code>php -S localhost:8000 -t C:\Users\dxbor\Documents\GitHub\mastering-of-php</code>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae debitis, deleniti dignissimos
+                doloribus dolorum eos harum in ipsa ipsam laboriosam perspiciatis provident quam quas qui quisquam quo
+                quos ratione rem rerum saepe sint tempora unde voluptates voluptatibus? Id maxime molestias sapiente
+                sequi. Assumenda cupiditate dignissimos, enim excepturi harum id incidunt ipsa iure, nobis obcaecati
+                officia pariatur perferendis quibusdam quisquam, voluptas! A alias assumenda atque beatae maxime modi
+                quod reiciendis voluptates. Ducimus eos et impedit voluptatibus. Cupiditate explicabo necessitatibus
+                omnis possimus tempora ullam voluptates. Accusamus aliquid, assumenda beatae consequatur consequuntur
+                cupiditate dolore dolorum fugit numquam odio officia quo, totam vero.</p>
+        </div>
+
+    </div>
+
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
+</body>
+</html>
