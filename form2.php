10a5a186b07538ab18b196329c36119409f23d51
irfan701 <irfanhossain701@gmail.com>
2023-03-09 00:25:33 +0600

    FILTER_SANITIZE_STRING

--- form2.php
+++ form2.php
@@ -31,60 +31,60 @@
             <code>XSS</code>
 
             <?php
-                $fname='';
-                $lname='';
+            $fname = '';
+            $lname = '';
 
             ?>
 
             <p>
-                <?php if(isset($_REQUEST['fname']) && !empty($_REQUEST['fname'])){ ?>
-
-              <?php  $fname=htmlspecialchars($_REQUEST['fname']) ?>
-
-                <?php }?>
-                <?php if(isset($_REQUEST['lname'])&& !empty($_REQUEST['lname'])){ ?>
-                    <?php $lname=htmlspecialchars($_REQUEST['lname']) ?>
-                <?php }?>
+                <?php if (isset($_REQUEST['fname']) && !empty($_REQUEST['fname'])) { ?>
+                    <!--              --><?php // $fname=htmlspecialchars($_REQUEST['fname']) ?>
+                    <?php $fname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING) ?>
+                <?php } ?>
+                <?php if (isset($_REQUEST['lname']) && !empty($_REQUEST['lname'])) { ?>
+                    <!--                    --><?php //$lname=htmlspecialchars($_REQUEST['lname']) ?>
+                    <?php $lname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING) ?>
+                <?php } ?>
             </p>
-<p>
-    First Name :<?php echo $fname ?>
-    <br/>
-    Last Name :<?php echo $lname ?>
-</p>
+            <p>
+                First Name :<?php echo $fname ?>
+                <br/>
+                Last Name :<?php echo $lname ?>
+            </p>
 
-        <div class="row">
-            <div class="col-md-6 card">
-                <form method="GET">
-                <div class="col-md-8">
-                    <div class="mb-3">
-                        <label class="form-label">First Name</label>
-                        <input type="text" name="fname" value="<?php echo $fname?>" class="form-control">
-                    </div>
-                </div>
-                <div class="col-md-8">
-                    <div class="mb-3">
-                        <label class="form-label">Last Name</label>
-                        <input type="text" name="lname" value="<?php echo $lname?>" class="form-control">
-                    </div>
-                </div>
-                <div class="col-md-8">
-                    <div class="mb-3">
+            <div class="row">
+                <div class="col-md-6 card">
+                    <form method="POST">
+                        <div class="col-md-8">
+                            <div class="mb-3">
+                                <label class="form-label">First Name</label>
+                                <input type="text" name="fname" value="<?php echo $fname ?>" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="mb-3">
+                                <label class="form-label">Last Name</label>
+                                <input type="text" name="lname" value="<?php echo $lname ?>" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="mb-3">
 
-                        <input type="submit" name="submit" class="btn btn-success">
-                    </div>
+                                <input type="submit" name="submit" class="btn btn-success">
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                </form>
+
             </div>
 
+
         </div>
-
-
     </div>
-</div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"></script>
 </body>
 </html>
