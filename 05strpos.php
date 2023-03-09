aa693d76b733aebf8582d96d4210490d533841ce
irfan701 <irfanhossain701@gmail.com>
2023-03-10 01:18:44 +0600

    strpos builtin function

---
+++ 05strpos.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ *
+ *  - স্ট্রিয়ের ভেতর স্ট্রিং সার্চ করা
+ */
+$string="The simple rat Rat exclaimed, This is amazing news! Friend, lead the way.";
+echo strpos($string,"rat"); //case sensitive
+echo "\n";
+
+echo stripos($string,"Rat"); //case insensitive
+echo "\n";
+
+echo strripos($string,"Rat"); //case insensitive
+echo "\n";
+
+
+
+$offset=strpos($string,"The");
+if ($offset!==false){   // AS 0 and false ,php te quivalent
+    echo "\n Word Was Found\n";
+}else{
+    echo "\n Word Was not Found\n";
+}
