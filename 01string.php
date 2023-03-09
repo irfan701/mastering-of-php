9e00753a06bbcd603cbfc287519dbd62553d0df5
irfan701 <irfanhossain701@gmail.com>
2023-03-10 01:16:55 +0600

    plain string & ASCII

---
+++ 01string.php
@@ -0,0 +1,14 @@
+<?php
+$name="IRFAN";
+echo "This is $name\n\t Say Hi";
+
+//ASCII
+echo "\n";
+echo  ord('A');
+echo PHP_EOL;
+echo  ord('a');
+echo PHP_EOL;
+echo  chr('13');
+echo  chr('98');
+
+echo "\n";
