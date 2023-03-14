02e34bcd5c303d74e83fad89e50fadc5b957a645
irfan701 <irfanhossain701@gmail.com>
2023-03-14 21:15:21 +0600

    nl2br()

---
+++ 09nl2br.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * nl2br()
+ */
+
+$string="Lorem ipsum dolor sit amet, consectetur adipisicing elit.\n Animi expedita hic magni maxime neque nisi porro quidem\n rerum sed voluptatibus IH!";
+echo $string;
+echo "<br>";
+echo nl2br($string);
+
